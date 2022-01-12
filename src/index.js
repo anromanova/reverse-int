28e9376e69002606947d7708921e334a0b2a3f59
Anastasia Romanova <anastasiya.erohina@gmail.com>
2022-01-12 23:47:16 +0000

    feat: tests pass

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-module.exports = function reverse(n) {
-    
-let reverseNew = n.toString().split('').reverse().join('');
-if (reverseNew < 0) {
-    return -reverseNew
-}
-return reverseNew
+module.exports = function reversedNum(num) {
+  return (
+    parseFloat(
+      num.toString().split('').reverse().join('')
+    )
+  ) 
 }
